66f6b2ad97cefc0e0fdd7de0c99ee965fec8d469
josephbertaux <jbertau@purdue.edu>
2022-11-01 16:18:50 -0400

    Sample Text

---
+++ compare_survey_data.cc
@@ -0,0 +1,103 @@
+void compare_survey_data()
+{
+	TFile* ogp_file = TFile::Open("intt_survey_data.root", "READ");
+	if(!ogp_file)return;
+	TTree* ogp_tree = (TTree*)ogp_file->Get("intt_survey_data");
+	if(!ogp_tree)return;
+
+	std::cout << "running..." << std::endl;
+	
+	TFile* file = TFile::Open("intt_geo.root", "RECREATE");
+	file->cd();
+	TTree* tree = new TTree("intt_geo", "intt_geo");
+
+	std::string name;
+	std::string* name_ptr = &name;
+	float mx, nx, dx;
+	float my, ny, dy;
+	float mz, nz, dz;
+
+	tree->Branch("name", &name);
+
+	tree->Branch("mx", &mx);	//measured x
+	tree->Branch("nx", &nx);	//nominal x
+	tree->Branch("dx", &dx);	//difference, measured - nominal
+
+	tree->Branch("my", &my);	//measured y
+	tree->Branch("ny", &ny);	//nominal y
+	tree->Branch("dy", &dy);	//difference, measured - nominal
+
+	tree->Branch("mz", &mz);	//measured z
+	tree->Branch("nz", &nz);	//nominal z
+	tree->Branch("dz", &dz);	//difference, measured - nominal
+
+	ogp_tree->SetBranchStatus("*", 0);
+
+	ogp_tree->SetBranchStatus("name", 1);
+	ogp_tree->SetBranchAddress("name", &name_ptr);
+
+	ogp_tree->SetBranchStatus("x", 1);
+	ogp_tree->SetBranchAddress("x", &mx);
+
+	ogp_tree->SetBranchStatus("y", 1);
+	ogp_tree->SetBranchAddress("y", &my);
+
+	ogp_tree->SetBranchStatus("z", 1);
+	ogp_tree->SetBranchAddress("z", &mz);
+
+	std::map<std::string, std::tuple<float, float, float>>::iterator itr;
+	std::map<std::string, std::tuple<float, float, float>> name_map;
+
+	name_map["snsr_A_crss_1"] = std::tuple<float, float, float>{13.50, 6.00, 0.0};
+	name_map["snsr_A_crss_2"] = std::tuple<float, float, float>{115.0, 6.00, 0.0};
+	name_map["snsr_A_crss_3"] = std::tuple<float, float, float>{115.0, 28.0, 0.0};
+	name_map["snsr_A_crss_4"] = std::tuple<float, float, float>{13.50, 28.0, 0.0};
+
+	name_map["snsr_B_crss_1"] = std::tuple<float, float, float>{115.7, 6.00, 0.0};
+	name_map["snsr_B_crss_2"] = std::tuple<float, float, float>{245.2, 6.00, 0.0};
+	name_map["snsr_B_crss_3"] = std::tuple<float, float, float>{245.2, 28.0, 0.0};
+	name_map["snsr_B_crss_4"] = std::tuple<float, float, float>{115.7, 28.0, 0.0};
+
+	name_map["snsr_C_crss_1"] = std::tuple<float, float, float>{246.8, 6.00, 0.0};
+	name_map["snsr_C_crss_2"] = std::tuple<float, float, float>{376.3, 6.00, 0.0};
+	name_map["snsr_C_crss_3"] = std::tuple<float, float, float>{376.3, 28.0, 0.0};
+	name_map["snsr_C_crss_4"] = std::tuple<float, float, float>{246.8, 28.0, 0.0};
+
+	name_map["snsr_D_crss_1"] = std::tuple<float, float, float>{377.0, 6.00, 0.0};
+	name_map["snsr_D_crss_2"] = std::tuple<float, float, float>{478.5, 6.00, 0.0};
+	name_map["snsr_D_crss_3"] = std::tuple<float, float, float>{478.5, 28.0, 0.0};
+	name_map["snsr_D_crss_4"] = std::tuple<float, float, float>{377.0, 28.0, 0.0};
+
+	name_map["ndcp_1"] = std::tuple<float, float, float>{0.000, 0.000, 0.0};
+	name_map["ndcp_2"] = std::tuple<float, float, float>{492.0, 0.000, 0.0};
+	name_map["ndcp_3"] = std::tuple<float, float, float>{492.0, 34.00, 0.0};
+	name_map["ndcp_4"] = std::tuple<float, float, float>{0.000, 34.00, 0.0};
+
+	size_t pos;
+	Long64_t n;
+	for(n = 0; n < ogp_tree->GetEntriesFast(); ++n)
+	{
+		ogp_tree->GetEntry(n);
+
+		for(itr = name_map.begin(); itr != name_map.end(); ++itr)
+		{
+			if(name.find(itr->first) != std::string::npos)
+			{
+				nx = std::get<0>(itr->second);
+				ny = std::get<1>(itr->second);
+				nz = std::get<2>(itr->second);
+
+				dx = mx - nx;
+				dy = my - ny;
+				dz = mz - nz;
+
+				tree->Fill();
+				break;
+			}
+		}
+	}
+
+	tree->Write();
+	file->Write();
+	file->Close();
+}
---
+++ draw_intt.cc
@@ -0,0 +1,73 @@
+TTree* tree = nullptr;
+TTree* temp = nullptr;
+float mx, nx, dx;
+float my, ny, dy;
+float mz, nz, dz;
+
+
+void draw_intt()
+{
+	TFile* file = TFile::Open("intt_geo.root", "READ");
+	if(!file)
+	{
+		std::cout << "Couldn't open 'intt_geo.root'" << std::endl;
+		return;
+	}
+
+	tree = (TTree*)file->Get("intt_geo");
+	if(!tree)
+	{
+		std::cout << "Couldn't get tree 'intt_gel'" << std::endl;
+		return;
+	}
+
+	tree->SetBranchStatus("mx", 1);	tree->SetBranchAddress("mx", &mx);
+	tree->SetBranchStatus("nx", 1);	tree->SetBranchAddress("nx", &nx);
+	tree->SetBranchStatus("dx", 1);	tree->SetBranchAddress("dx", &dx);
+
+	tree->SetBranchStatus("my", 1);	tree->SetBranchAddress("my", &my);
+	tree->SetBranchStatus("ny", 1);	tree->SetBranchAddress("ny", &ny);
+	tree->SetBranchStatus("dy", 1);	tree->SetBranchAddress("dy", &dy);
+
+	tree->SetBranchStatus("mz", 1);	tree->SetBranchAddress("mz", &mz);
+	tree->SetBranchStatus("nz", 1);	tree->SetBranchAddress("nz", &nz);
+	tree->SetBranchStatus("dz", 1);	tree->SetBranchAddress("dz", &dz);
+}
+
+void DrawComponent(std::string s, std::string expr = "my:mx", std::string opt = "")
+{
+	if(!tree)return;
+	if(s == "")return;
+
+	int width = 0;
+
+	if(temp)delete temp;
+	temp = new TTree(s.c_str(), s.c_str());
+	temp->SetMarkerStyle(5); //(52 + width * 18); //50 + width * 18, 51 + width * 18, 52 + width * 18, ...
+	temp->Branch("mx", &mx);
+	temp->Branch("nx", &nx);
+	temp->Branch("dx", &dx);
+	
+	temp->Branch("my", &my);
+	temp->Branch("ny", &ny);
+	temp->Branch("dy", &dy);
+	
+	temp->Branch("mz", &mz);
+	temp->Branch("nz", &nz);
+	temp->Branch("dz", &dz);
+
+	std::string* name = new std::string;
+	tree->SetBranchStatus("name", 1);	tree->SetBranchAddress("name", &name);
+	for(Long64_t n = 0; n < tree->GetEntriesFast(); n++)
+	{
+		tree->GetEntry(n);
+
+		if(name->find(s) != std::string::npos)
+		{
+			temp->Fill();
+		}
+	}	
+	delete name;
+
+	temp->Draw(expr.c_str(), "", opt.c_str());
+}
