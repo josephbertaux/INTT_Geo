7d150483839374aa8ff41ba68663f198f7c222c3
josephbertaux <jbertau@purdue.edu>
2022-11-10 12:25:34 -0500

    Sample Text

--- draw_transforms.cc
+++ draw_transforms.cc
@@ -6,136 +6,100 @@ void draw_transforms()
 	TTree* tree = (TTree*)file->Get("intt_transforms");
 	if(!tree){std::cout << "tree" << std::endl;return;}
 
+
+	std::vector<std::string> sensors = {"snsr_A", "snsr_B", "snsr_C", "snsr_D"};
+	std::map<std::string, std::tuple<float*, float*, float*, Long64_t*, TH1F*>> params =
+	{
+				//address	//mean		//std. dev.	//counts
+		{"dx_m",	{new float(0),	new float(0),	new float(0),	new Long64_t(0), nullptr}},
+		{"dy_m",	{new float(0),	new float(0),	new float(0),	new Long64_t(0), nullptr}},
+		{"dz_m",	{new float(0),	new float(0),	new float(0),	new Long64_t(0), nullptr}},
+		{"a_m",		{new float(0),	new float(0),	new float(0),	new Long64_t(0), nullptr}},
+		{"b_m",		{new float(0),	new float(0),	new float(0),	new Long64_t(0), nullptr}},
+		{"g_m",		{new float(0),	new float(0),	new float(0),	new Long64_t(0), nullptr}},
+
+		{"dx_r",	{new float(0),	new float(0),	new float(0),	new Long64_t(0), nullptr}},
+		{"dy_r",	{new float(0),	new float(0),	new float(0),	new Long64_t(0), nullptr}},
+		{"dz_r",	{new float(0),	new float(0),	new float(0),	new Long64_t(0), nullptr}},
+		{"a_r",		{new float(0),	new float(0),	new float(0),	new Long64_t(0), nullptr}},
+		{"b_r",		{new float(0),	new float(0),	new float(0),	new Long64_t(0), nullptr}},
+		{"g_r",		{new float(0),	new float(0), 	new float(0),	new Long64_t(0), nullptr}}
+	};
+
 	std::string name;
 	std::string* name_ptr = &name;
 	tree->SetBranchStatus("name", 1);
 	tree->SetBranchAddress("name", &name_ptr);
-
-	float dx_m;
-	float dy_m;
-	float dz_m;
-	float a_m;
-	float b_m;
-	float g_m;
-
-	float dx_r;
-	float dy_r;
-	float dz_r;
-	float a_r;
-	float b_r;
-	float g_r;
-	std::vector<std::string> sensors = {"snsr_A", "snsr_B", "snsr_C", "snsr_D"};
-	std::map<std::string, float*> params =
-	{
-		{"dx_m",	&dx_m},
-		{"dy_m",	&dy_m},
-		{"dz_m",	&dz_m},
-		{"a_m",		&a_m},
-		{"b_m",		&b_m},
-		{"g_m",		&g_m},
-
-		{"dx_r",	&dx_r},
-		{"dy_r",	&dy_r},
-		{"dz_r",	&dz_r},
-		{"a_r",		&a_r},
-		{"b_r",		&b_r},
-		{"g_r",		&g_r}
-	};
-
-	std::string temp;
-
 	for(auto par = params.begin(); par != params.end(); ++par)
 	{
-		float param_m = 0.0;
-		float param_v = 0.0;
-
 		tree->SetBranchStatus(par->first.c_str(), 1);
-		tree->SetBranchAddress(par->first.c_str(), par->second);
+		tree->SetBranchAddress(par->first.c_str(), std::get<0>(par->second));
+	}
 
-		Long64_t N = 0;
+	for(auto snsr = sensors.begin(); snsr != sensors.end(); ++snsr)
+	{
+		for(auto par = params.begin(); par != params.end(); ++par)
+		{
+			*std::get<1>(par->second) = 0;	//mean
+			*std::get<2>(par->second) = 0;	//variance
+			*std::get<3>(par->second) = 0;	//counts
+		}
+
 		for(Long64_t n = 0; n < tree->GetEntriesFast(); ++n)
 		{
 			tree->GetEntry(n);
 
 			//bad point--OGP machine failed to find one of the crosses
 			if(name.find("B1L114") != std::string::npos)continue;
+			if(name.find(*snsr) == std::string::npos)continue;
 
-			param_m += *par->second;
-			param_v += (*par->second) * (*par->second);
-
-			++N;
-		}
-
-		param_m /= N;
-		param_v /= N;
-		param_v = sqrt(param_v - param_m * param_m);
-	
-		std::cout << std::endl;	
-		std::cout << par->first << ":" << std::endl;
-		std::cout << "\t" << param_m << std::endl;
-		std::cout << "\t" << param_v << std::endl;
-
-		TCanvas* c1; 
-		TH1F* h = new TH1F("h", par->first.c_str(), 9, param_m - 4.0 * param_v, param_m + 4.0 * param_v);
-
-		for(int j = 0; j < sensors.size(); ++j)
-		{
-			h->Reset();
-
-			param_m = 0;
-			param_v = 0;
-
-			N = 0;
-			for(Long64_t n = 0; n < tree->GetEntriesFast(); ++n)
+			for(auto par = params.begin(); par != params.end(); ++par)
 			{
-				tree->GetEntry(n);
-	
-				//bad point--OGP machine failed
-				if(name.find("B1L114") != std::string::npos)continue;
-				if(name.find(sensors[j]) == std::string::npos)continue;
+				*std::get<1>(par->second) += (*std::get<0>(par->second));
+				*std::get<2>(par->second) += (*std::get<0>(par->second)) * (*std::get<0>(par->second));
 
-				h->Fill(*par->second);	
-				param_m += *par->second;
-				param_v += (*par->second) * (*par->second);
-	
-				++N;
+				*std::get<3>(par->second) += 1;
 			}
-
-			param_m /= N;
-			param_v /= N;
-			param_v = sqrt(param_v - param_m * param_m);
-
-			std::cout << std::endl;	
-			std::cout << "\t" << sensors[j] << ":" << std::endl;
-			std::cout << "\t\t" << param_m << std::endl;
-			std::cout << "\t\t" << param_v << std::endl;
-
-			h->Draw();
-
-			c1 = (TCanvas*)gROOT->FindObject("c1");
-			temp = "param_plots/";
-			temp += par->first + "_" + sensors[j] + ".png";
-			if(c1)c1->SaveAs(temp.c_str());
 		}
 
-		h->Reset();
+		std::cout << *snsr << std::endl;
+		for(auto par = params.begin(); par != params.end(); ++par)
+		{
+			float m = (*std::get<1>(par->second)) / (*std::get<3>(par->second));
+			float s = (*std::get<2>(par->second)) / (*std::get<3>(par->second));
+			s = sqrt((s - m * m) > 0 ? s - m * m : m * m - s);
+	
+			name = *snsr + "_" + par->first;	
+			std::get<4>(par->second) = new TH1F(name.c_str(), name.c_str(), 9, m - 4.0 * s, m + 4.0 * s);
+
+			std::cout << "\t" << par->first << std::endl;
+			std::cout << "\t\tm:\t" << m << std::endl;
+			std::cout << "\t\ts:\t" << s << std::endl;
+		}
 
 		for(Long64_t n = 0; n < tree->GetEntriesFast(); ++n)
 		{
 			tree->GetEntry(n);
 
-			//bad point--OGP machine failed
+			//bad point--OGP machine failed to find one of the crosses
 			if(name.find("B1L114") != std::string::npos)continue;
+			if(name.find(*snsr) == std::string::npos)continue;
 
-			h->Fill(*par->second);	
+			for(auto par = params.begin(); par != params.end(); ++par)
+			{
+				std::get<4>(par->second)->Fill(*std::get<0>(par->second));
+			}
 		}
 
-		h->Draw();
-		c1 = (TCanvas*)gROOT->FindObject("c1");
-		temp = "param_plots/";
-		temp += par->first + ".png";
-		if(c1)c1->SaveAs(temp.c_str());
+		for(auto par = params.begin(); par != params.end(); ++par)
+		{
+			std::get<4>(par->second)->Draw();
 
-		delete h;
+			TCanvas* c1 = (TCanvas*)gROOT->FindObject("c1");
+			name = "param_plots/";
+			name += *snsr + "_" + par->first + ".png";
+
+			if(c1)c1->SaveAs(name.c_str());
+		}
 	}
 }
